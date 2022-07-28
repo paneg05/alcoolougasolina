4cce57086365d7ca3c36340eb6d0cc6bebe3860c
luan santos <aluncard6@gmail.com>
2022-07-27 22:11:42 -0300

    initial commit

---
+++ alcool_ou_gasolina/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'alcool_ou_gasolina',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
